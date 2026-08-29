d36d61a6d3dba5afb1637f008239d95795a25d86
AmgdGocha <amgdgocha53@gmail.com>
2026-08-29 16:59:06 +0300

    enhancing the file structure.
    enhancing the file structure

--- drivefs_sleuth.py
+++ drivefs_sleuth.py
@@ -4,8 +4,13 @@ Email: amged_wageh@outlook.com
 LinkedIn: https://www.linkedin.com/in/amgedwageh/
 """
 
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
+
 from drivefs_sleuth.executor import execute
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     execute()
